f42c95612e9cbe961c57e53aff13bf3d8f0a1364
b63 <avink5760@gmail.com>
2020-05-04 10:31:40 -0500

    moved chrome ext files under chrome, again

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener((details) => {
-  if (details.reason.search(/install/g) === -1) {
-    return;
-  }
-  chrome.tabs.create({
-    url: chrome.extension.getURL('welcome.html'),
-    active: true
-  });
-});
